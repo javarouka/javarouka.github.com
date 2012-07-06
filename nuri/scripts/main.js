1b1f703e6990124599cf7e7734a2ae689dbf4258
Yi Hanghee <javarouka@gmail.com>
2012-07-06 14:02:07 +0900

    change dependency change

--- nuri/scripts/main.js
+++ nuri/scripts/main.js
@@ -1,18 +1,23 @@
-require([
-    "javarouka/utils/Logger"
-], function(Logger) {
+require([ "/ext-3.4.0/adapter/ext/ext-base"], function() {
 
-	var CONTEXT = window.GLOBAL_CONTEXT;
-	
-	var ControllerMap = {
-		List: "ListController",
-		Detail: "DetailController"
-	}
-	
-	var controller = ControllerMap[GLOBAL_CONTEXT.GADGET];
-	
-	require([ 'javarouka/controller/' + controller ], function(Controller) {
-		Controller.execute(CONTEXT);
+	require([
+		"scripts/ext-3.4.0/ext-all",
+	    "javarouka/utils/Logger"
+	], function(Logger) {
+
+		var CONTEXT = window.GLOBAL_CONTEXT;
+		
+		var ControllerMap = {
+			List: "ListController",
+			Detail: "DetailController"
+		}
+		
+		var controller = ControllerMap[GLOBAL_CONTEXT.GADGET];
+		
+		require([ 'javarouka/controller/' + controller ], function(Controller) {
+			Controller.execute(CONTEXT);
+		});
+		
 	});
 	
-});
+})
