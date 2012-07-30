31d2a08622d26d4bf3133efde4cca61a91bea921
Yi Hanghee <javarouka@gmail.com>
2012-07-30 15:32:58 +0900

    add

--- nuri/scripts/robotModules/save.js
+++ nuri/scripts/robotModules/save.js
@@ -11,8 +11,18 @@ define([
 			Logger.error("이미 출동했습니다.");
 			return;
 		}
+		if($robot.size() < 1 || $robot.find("div.parts") < 3) {
+			Logger.error("출격할 로봇이 미완성이거나 없습니다.");
+			return;
+		}
 
 		Logger.log("출동합니다.");
+		
+		$robot.css("position", "relative").animate({
+			right: -9000,
+			bottom: -5000
+		}, 2000);		
+
 		executed = true;
 	};
 
