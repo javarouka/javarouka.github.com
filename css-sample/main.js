352cb0ca27d0f815615d9bb329501884dd8e9b73
Yi Hanghee <javarouka@gmail.com>
2012-09-03 13:15:35 +0900

    .

--- css-sample/main.js
+++ css-sample/main.js
@@ -1,17 +1,14 @@
 require.config({ 
 	baseUrl: '/css-sample/',	
  	paths : {
- 		syntaxCore: "http://agorbatchev.typepad.com/pub/sh/3_0_83/scripts/shCore",
- 		syntaxLoader: "http://agorbatchev.typepad.com/pub/sh/3_0_83/scripts/shAutoloader",
+ 		order: "lib/order",
 		jquery: 'lib/jquery',
 		javarouka: "me/javarouka"
 	}
 });
 
-require([ 
-	"javarouka/App"
-], function(App) {
-	
+require([ "javarouka/App" ], function(App) {
+
 	App.init();
-	
+
 });
--- css-sample/me/javarouka/App.js
+++ css-sample/me/javarouka/App.js
@@ -5,12 +5,31 @@ define([
 
 	var version = "0.1.0";
 
-	var syntaxHighlight = function() {		
-	}
+	var initializeUI = function() {
+		$("dl dd a.apply-button").each(function() {
+			var $btn = $(this);
+			var text = $btn.parent("span").data("selector");
+			$btn.text("Selector : " + text);
+		});
+	};
+
+	var syntaxHighlight = function() {	
+		SyntaxHighlighter.all();
+	};
+
+	var eventBinding = function() {
+		$("section dl").on("click", "a.apply-button", function(e) {		
+			e.preventDefault();	
+			var selector = $(e.target).parent("span").data("selector");	
+			var $ctx = $(this).parents("dd");			
+			$ctx.find(selector).addClass($ctx.prev("dt").attr("id"));
+		});
+	};
 
 	var init = function() {
+		initializeUI();
 		syntaxHighlight();
-		return this;
+		eventBinding();
 	};
 
 	return $.extend(parent, {
