2e9354d7d24c6c2a9c17a199e013e70c44d6da23
Yi Hanghee <javarouka@gmail.com>
2012-09-04 14:12:33 +0900

    .

--- css-sample/me/javarouka/App.js
+++ css-sample/me/javarouka/App.js
@@ -6,23 +6,41 @@ define([
 ], function($, parent, Context, syntaxh) {	
 
 	var version = "0.1.0";
-
-	var initializeUI = function() {
-		$("dl dd a.apply-button").each(function() {
-			var $btn = $(this);
-			var text = $btn.parent("span").data("selector");
-			$btn.text("Selector : " + text);
-		});
-		syntaxh.highlight();
+	var G = {
+		highlightClass: "highlight",
+		$ELEMENTS: {}
 	};
 
-	var eventBinding = function() {		
-		$("section dl").on("click", "a.apply-button", function(e) {		
-			e.preventDefault();	
-			var selector = $(e.target).parent("span").data("selector");	
-			var $ctx = $(this).parents("dd");			
-			$ctx.find(selector).addClass($ctx.prev("dt").attr("id"));
-		});
+	var initializeUI = function() {		
+		syntaxh.highlight();
+		G.$ELEMENTS = {
+			findContext: $("section .target"),
+			controlBar: $(".ctrl-bar"),
+			selectorString: $("#selector-string")
+		};
+	};
+
+	var clear = function() {
+		G.$ELEMENTS.findContext.find("." + G.highlightClass)
+			.removeClass(G.highlightClass);
+		G.$ELEMENTS.selectorString.val("");
+	};
+
+	var eventBinding = function() {
+		$(document.body).on("click", clear);
+
+		G.$ELEMENTS.controlBar.on(
+			"keyup",
+			G.$ELEMENTS.selectorString,
+			function(e) {
+				if(e) {
+					e.stopPropagation();					
+					var selector = e.target.value;
+					var t = G.$ELEMENTS.findContext.find(selector);
+					t.addClass(G.highlightClass);
+				}
+			}
+		);
 	};
 
 	var init = function() {
