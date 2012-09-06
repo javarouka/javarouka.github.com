aa7bd5778a73bb78f25db23ca39da75a70b264dc
Yi Hanghee <javarouka@gmail.com>
2012-09-06 11:14:55 +0900

    .

--- css-sample/me/javarouka/App.js
+++ css-sample/me/javarouka/App.js
@@ -6,24 +6,41 @@ define([
 ], function($, parent, Context, syntaxh) {	
 
 	var version = "0.1.0";
+
+	var modifyInterval = 1000;
 	var G = {
 		highlightClass: "highlight",
 		$ELEMENTS: {}
 	};
 
-	var initializeUI = function() {		
-		syntaxh.highlight();
-		G.$ELEMENTS = {
+	var initializeUI = function() {						
+		var $el =  {
+			syntaxing: $("#syntaxing"),
 			findContext: $("section .target"),
 			controlBar: $(".ctrl-bar"),
 			selectorString: $("#selector-string")
 		};
+		$el.syntaxing.text($el.findContext.html());
+		syntaxh.highlight();
+		return $el;
 	};
+
+	var doHighlighting = function(selector) {
+		clear();
+		try {
+			var t = G.$ELEMENTS.findContext.find(selector);
+			if(t && t.size() > 0) {
+				t.addClass(G.highlightClass);
+			}
+		}	
+		catch(jQueryError) {
+			clear();
+		}
+	}
 
 	var clear = function() {
 		G.$ELEMENTS.findContext.find("." + G.highlightClass)
 			.removeClass(G.highlightClass);
-		G.$ELEMENTS.selectorString.val("");
 	};
 
 	var eventBinding = function() {
@@ -32,19 +49,24 @@ define([
 		G.$ELEMENTS.controlBar.on(
 			"keyup",
 			G.$ELEMENTS.selectorString,
-			function(e) {
+			function(e) {				
 				if(e) {
-					e.stopPropagation();					
-					var selector = e.target.value;
-					var t = G.$ELEMENTS.findContext.find(selector);
-					t.addClass(G.highlightClass);
+					e.stopPropagation();
+					if(modifyInterval) {
+						clearTimeout(modifyInterval);
+					}
+					modifyInterval = setTimeout(
+						doHighlighting, 
+						modifyInterval, 
+						e.target.value
+					);					
 				}
 			}
 		);
 	};
 
 	var init = function() {
-		initializeUI();
+		G.$ELEMENTS = initializeUI();
 		eventBinding();
 	};
 
