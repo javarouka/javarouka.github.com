b25621113a2adbda839655f2eeba154c480b5dde
이항희 <javarouka@nuritelecom.com>
2013-06-11 11:35:09 +0900

    .

--- ecmascript5-sample/json.js
+++ ecmascript5-sample/json.js
@@ -1,21 +1,30 @@
-// 문자열.
-var jsonString = "{ \"key\":\"value\" }";
+(function() {
 
-// eval 사용
-var case1 = eval("("+jsonString+")");
+  // 문자열.
+  var jsonString = "{ \"key\":\"value\" }";
 
-// 동적 함수 사용
-var converter = new Function(" return " + jsonString + "; ");
-var case2 = converter();
+  // eval 사용
+  var case1 = eval("("+jsonString+")");
+  console.log("case1", case1);
 
-// ecmascript 5
-var case3 = JSON.parse(jsonString);
+  // 동적 함수 사용
+  var converter = new Function(" return " + jsonString + "; ");
+  var case2 = converter();
+  console.log("case2", case2);
 
-// 문자열화 - 속성을 열거하면서 직접 구현해야만 했다!
-var serialize = function(obj) {
-  //https://github.com/douglascrockford/JSON-js/blob/master/json2.js
-  // implements...
-};
+  // ecmascript 5
+  var case3 = JSON.parse(jsonString);
+  console.log("case3", case3);
 
-// 초간단.
-jsonString = JSON.stringify(case3);
+  // 문자열화 - 속성을 열거하면서 직접 구현해야만 했다!
+  var serialize = function(obj) {
+    //https://github.com/douglascrockford/JSON-js/blob/master/json2.js
+    // implements...
+  };
+
+  // 초간단.
+  jsonString = JSON.stringify(case3);
+
+  console.log("json string", jsonString);
+
+})();
