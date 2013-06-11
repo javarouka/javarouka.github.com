d6f6fc5966f88dd404a04ff66e45bdf52935bd94
이항희 <javarouka@nuritelecom.com>
2013-06-11 11:35:44 +0900

    .

--- ecmascript5-sample/object.prototype.js
+++ ecmascript5-sample/object.prototype.js
@@ -1,23 +1,27 @@
-var current = new Date();
+(function() {
 
-// ES3
-console.log(current.__proto__ === Date.prototype);
-console.log(current.constructor.prototype === Date.prototype);
+  var current = new Date();
 
-// ES5
-console.log(Object.getPrototypeOf(current) === Date.prototype);
+  // ES3
+  console.log(current.__proto__ === Date.prototype);
+  console.log(current.constructor.prototype === Date.prototype);
+
+  // ES5
+  console.log(Object.getPrototypeOf(current) === Date.prototype);
 
 
-// java의 super로 활용하기
-function StudyGroup(){}
-StudyGroup.prototype.study = function(something) {
-  console.log(something + " study");
-};
+  // java의 super로 활용하기
+  function StudyGroup(){}
+  StudyGroup.prototype.study = function(something) {
+    console.log(something + " study");
+  };
 
-function Octobersky(){}
+  function Octobersky(){}
 
-Octobersky.prototype = new StudyGroup();
+  Octobersky.prototype = new StudyGroup();
 
-Octobersky.prototype.study = function(){
-  Object.getPrototypeOf(this).study("jsvascript");
-};
+  Octobersky.prototype.study = function(){
+    Object.getPrototypeOf(this).study("javascript");
+  };
+
+})();
