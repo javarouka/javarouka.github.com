b5febe8801a80e32a01fe70681c21a4a9ac5b9bb
이항희 <javarouka@nuritelecom.com>
2013-06-19 16:49:03 +0900

    include extjs mvc study source

---
+++ hello-extmvc/app.js
@@ -0,0 +1,21 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: javarouka
+ * Date: 13. 6. 19
+ * Time: 오후 4:44
+ * To change this template use File | Settings | File Templates.
+ */
+Ext.Loader.setConfig({enabled: true});
+
+Ext.application({
+
+  name: 'LoveMeExt',
+
+  autoCreateViewport: true,
+
+  launch: function() {
+    // app 이 ready 되었을때
+    console.log("app launch!");
+  }
+
+});
---
+++ hello-extmvc/app/view/Viewport.js
@@ -0,0 +1,11 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: javarouka
+ * Date: 13. 6. 19
+ * Time: 오후 4:46
+ * To change this template use File | Settings | File Templates.
+ */
+Ext.define("App.view.Viewport", {
+  extend: "Ext.container.Viewport",
+  layout: "fit"
+});
