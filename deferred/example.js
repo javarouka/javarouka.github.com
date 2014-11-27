366ace8ca08a938c9dad8d5e4061a8cc4dbfa182
javarouka(이항희) <javarouka@coupang.com>
2014-11-27 16:58:12 +0900

    ..

--- deferred/example.js
+++ deferred/example.js
@@ -1,15 +1,33 @@
+function wait(time) {
+    var def = $.Deferred();
+    setTimeout(def.resolve, time);
+    return def.promise();
+}
+
 function random(_default) {
     return ~~(Math.random() * (_default || 10) * 1000);
 }
 
-function A(name) {
+function Tester(name, rejectLimit) {
     var def = $.Deferred(), r = random(), name = name || "anonymouse-" + (+new Date);
-    def.notify("["+name+"] 준비");
+    def.notify("["+name+"] progressing... 0%");
     setTimeout(function() {
-        def.resolve("["+name+"] " + r);
+        def.resolve("["+name+"] resolve. ## " + r + " millis.");
     }, r);
     setTimeout(function() {
-        def.notify("["+name+"] 반절 지남");
-    }, r/2)
+        def.notify("["+name+"] progressing... 50%");
+    }, r/2);
+    setTimeout(function() {
+        def.reject("["+name+"] reject! ## " + r + " millis.");
+    }, rejectLimit || 5000);
     return def.promise();
 }
+
+
+Tester("First").progress(function(r){ console.log(r) })
+    .done(function(r){ console.log(r) })
+    .fail(function(r){ console.log(r) });
+
+Tester("Second").progress(function(r){ console.log(r) })
+    .done(function(r){ console.log(r) })
+    .fail(function(r){ console.log(r) });
