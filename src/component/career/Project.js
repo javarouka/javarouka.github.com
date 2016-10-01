2dfcdc77b6c3493ef18ad77e65e29d8a4823b0f2
javarouka <javarouka@coupang.com>
2016-10-01 20:01:02 +0900

    #3 프로젝트 업데이트

--- src/component/career/Project.js
+++ src/component/career/Project.js
@@ -1,12 +1,16 @@
 import React from 'react'
-
+const deprecated = {
+    textDecoration: 'line-through',
+    color: 'gray'
+};
 export const Project = ({ children }) => (
     <div>
         <h2>Project</h2>
         <nav>
             <ul>
+                <li><a href="//github.com/javarouka/decorator-advice" target="_blank">decorator advice</a></li>
                 <li><a href="//github.com/tipjs/tipjs-JavaScript-MVC-Framework" target="_blank">tipJS (Client-Side MVC Framework)</a></li>
-                <li><a href="//javarouka.github.com/crosscutting.js" target="_blank"><span>crosscutting.js (JavaScript AOP Library)</span></a></li>
+                <li><a  style={deprecated} href="//javarouka.github.com/crosscutting.js" target="_blank"><span>crosscutting.js (JavaScript AOP Library)</span></a></li>
             </ul>
         </nav>
     </div>
