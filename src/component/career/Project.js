0a67aa1e20bf5d3d6753993aff398819cff4d900
javarouka <javarouka@coupang.com>
2016-10-08 19:19:23 +0900

    #3 프로젝트 업데이트

--- src/component/career/Project.js
+++ src/component/career/Project.js
@@ -8,6 +8,7 @@ export const Project = ({ children }) => (
         <h2>Project</h2>
         <nav>
             <ul>
+                <li><a href="//github.com/javarouka/class-method-hook" target="_blank">decorator class method hook</a></li>
                 <li><a href="//github.com/javarouka/decorator-advice" target="_blank">decorator advice</a></li>
                 <li><a href="//github.com/tipjs/tipjs-JavaScript-MVC-Framework" target="_blank">tipJS (Client-Side MVC Framework)</a></li>
                 <li><a  style={deprecated} href="//javarouka.github.com/crosscutting.js" target="_blank"><span>crosscutting.js (JavaScript AOP Library)</span></a></li>
